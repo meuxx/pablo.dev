deac5b9798284b87f357998b0d171b81ea4912c0
Pablo Maceda <contact@pablo.dev>
2020-11-19 04:25:48 +0000

    Reduce bundle size

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
 
 import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 config.autoAddCss = false
-library.add(fab, faEnvelope)
+library.add(faGithub, faLinkedin, faEnvelope)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
