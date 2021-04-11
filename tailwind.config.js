f437c073919978d8cad7c09f0c4e97aacf74a977
Pablo Maceda <contact@pablo.dev>
2021-04-11 17:40:05 +0100

    Enable taildwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   darkMode: false,
   theme: {
